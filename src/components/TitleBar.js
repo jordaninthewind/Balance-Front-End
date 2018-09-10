c717f97ef26cb1bcb25b51b470ccfd348333e43d
Jordan Kline <jordan.kline@gmail.com>
2018-09-10 16:20:09 -0700

    added responsive sun graphic to title component

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -3,7 +3,8 @@ import React from 'react';
 const TitleBar = () => {
   return (
 	  <div className="App-header">
-	  	<h1>B a l a n c e : M e d i t a t i o n</h1>
+
+	  	<div><img src="shambhala sun.png" alt="" className="responsiveImage"/><h1 className="title">B a l a n c e : M e d i t a t i o n</h1></div>
 	  </div>
   );
 };
